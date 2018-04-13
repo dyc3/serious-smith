719c89fb8ce7dea6649532f69a84c3406c49f2e3
Carson McManus <carson.mcmanus1@gmail.com>
2018-04-13 12:39:23 -0400

    finish project setup

---
+++ game/src/main/java/GameRunner.java
@@ -0,0 +1,28 @@
+package game;
+
+import com.almasb.fxgl.app.GameApplication;
+import com.almasb.fxgl.entity.Entities;
+import com.almasb.fxgl.entity.Entity;
+import com.almasb.fxgl.input.Input;
+import com.almasb.fxgl.input.UserAction;
+import com.almasb.fxgl.settings.GameSettings;
+import javafx.scene.input.KeyCode;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.text.Text;
+import java.util.Map;
+
+public class GameRunner extends GameApplication {
+
+    @Override
+    protected void initSettings(GameSettings settings) {
+        settings.setWidth(600);
+        settings.setHeight(600);
+        settings.setTitle("Basic Game App");
+        settings.setVersion("0.1");
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
