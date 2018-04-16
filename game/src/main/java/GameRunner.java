d2f78649708a48e9a5f91850ded6cb9d5fcd947a
Carson McManus <carson.mcmanus1@gmail.com>
2018-04-16 09:56:40 -0400

    add basic player

--- game/src/main/java/GameRunner.java
+++ game/src/main/java/GameRunner.java
@@ -25,4 +25,66 @@ public class GameRunner extends GameApplication {
     public static void main(String[] args) {
         launch(args);
     }
+
+    private Entity player;
+
+    @Override
+    protected void initGame() {
+        player = Entities.builder()
+                .at(300, 300)
+                .viewFromNode(new Rectangle(25, 25, Color.BLUE))
+                .buildAndAttach(getGameWorld());
+    }
+
+    @Override
+    protected void initInput() {
+        Input input = getInput();
+
+        input.addAction(new UserAction("Move Right") {
+            @Override
+            protected void onAction() {
+                player.translateX(5); // move right 5 pixels
+                getGameState().increment("pixelsMoved", +5);
+            }
+        }, KeyCode.D);
+
+        input.addAction(new UserAction("Move Left") {
+            @Override
+            protected void onAction() {
+                player.translateX(-5); // move left 5 pixels
+                getGameState().increment("pixelsMoved", +5);
+            }
+        }, KeyCode.A);
+
+        input.addAction(new UserAction("Move Up") {
+            @Override
+            protected void onAction() {
+                player.translateY(-5); // move up 5 pixels
+                getGameState().increment("pixelsMoved", +5);
+            }
+        }, KeyCode.W);
+
+        input.addAction(new UserAction("Move Down") {
+            @Override
+            protected void onAction() {
+                player.translateY(5); // move down 5 pixels
+                getGameState().increment("pixelsMoved", +5);
+            }
+        }, KeyCode.S);
+    }
+
+    @Override
+    protected void initUI() {
+        Text textPixels = new Text();
+        textPixels.setTranslateX(50); // x = 50
+        textPixels.setTranslateY(100); // y = 100
+
+        textPixels.textProperty().bind(getGameState().intProperty("pixelsMoved").asString());
+        getGameScene().addUINode(textPixels); // add to the scene graph
+    }
+
+    @Override
+    protected void initGameVars(Map<String, Object> vars) {
+        vars.put("pixelsMoved", 0);
+    }
 }
