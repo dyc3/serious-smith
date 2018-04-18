80cc6716bfdb85fffbb3bc8be69f517b93292280
Carson McManus <carson.mcmanus1@gmail.com>
2018-04-18 12:52:31 -0400

    add framework for interval between boss attacks

--- game/src/main/java/BossControl.java
+++ game/src/main/java/BossControl.java
@@ -6,9 +6,37 @@ import com.almasb.fxgl.entity.Entity;
 public class BossControl extends Control {
     private double time;
 
+    /** The minimum amount of time between attacks in seconds. Actual attack intervals may vary
+     * depending on previous attack performed. **/
+    private double baseAttackInterval;
+    private double timeUntilAttack;
+
+    public BossControl()
+    {
+        this.baseAttackInterval = 2;
+        this.timeUntilAttack = baseAttackInterval;
+    }
+
+    public BossControl(double baseAttackInterval)
+    {
+        this.baseAttackInterval = baseAttackInterval;
+        this.timeUntilAttack = baseAttackInterval;
+    }
+
     @Override
-    public void onUpdate(Entity entity, double tpf) {
+    public void onUpdate(Entity entity, double tpf)
+    {
         time += tpf;
         entity.translateX(Math.sin(time));
+
+        if (timeUntilAttack <= 0)
+        {
+            // TODO: do attack
+            timeUntilAttack = baseAttackInterval;
+        }
+        else
+        {
+            timeUntilAttack -= tpf;
+        }
     }
 }
