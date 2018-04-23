d0b054507035e3fc53ab399f4dbbfc6cdfc26916
Carson McManus <carson.mcmanus1@gmail.com>
2018-04-23 14:22:00 -0400

    add dashing, dash key = shift

--- game/src/main/java/PlayerControl.java
+++ game/src/main/java/PlayerControl.java
@@ -16,9 +16,16 @@ import main.java.ProjectileControl;
 
 public class PlayerControl extends Control
 {
+    /** Movement speed. **/
     private final static double DEFAULT_MOVE_SPEED = 200;
+    /** Time between auto fire in seconds. **/
     private final static double DEFAULT_FIRE_INTERVAL = 0.25;
+    /** Distance to travel while dashing. **/
     private final static double DASH_DISTANCE = 200;
+    /** Speed while dashing. **/
+    private final static double DASH_SPEED = 1200;
+    /** Minimum time between dashes in seconds. **/
+    private final static double DASH_COOLDOWN = 1;
 
     private double speed;
     private Input input;
@@ -27,11 +34,38 @@ public class PlayerControl extends Control
 
     private Entity boss;
 
+    private boolean dashing = false;
+    private Point2D dashTarget;
+    private double dashCooldown = 0;
+
     public PlayerControl(Input input)
     {
         speed = DEFAULT_MOVE_SPEED;
         fireInterval = DEFAULT_FIRE_INTERVAL;
         this.input = input;
+    }
+
+    private Point2D getMoveDirection()
+    {
+        int x = 0, y = 0;
+        if (input.isHeld(KeyCode.W))
+        {
+            y = -1;
+        }
+        else if (input.isHeld(KeyCode.S))
+        {
+            y = 1;
+        }
+
+        if (input.isHeld(KeyCode.A))
+        {
+            x = -1;
+        }
+        else if (input.isHeld(KeyCode.D))
+        {
+            x = 1;
+        }
+        return new Point2D(x, y);
     }
 
     @Override
@@ -42,23 +76,36 @@ public class PlayerControl extends Control
             boss = getEntity().getWorld().getEntityByID("boss", 0).get();
         }
 
-        double moveDistance = speed * tpf;
-        if (input.isHeld(KeyCode.W))
+        if (input.isHeld(KeyCode.SHIFT) && !dashing && dashCooldown <= 0)
         {
-            entity.translateY(-moveDistance);
-        }
-        else if (input.isHeld(KeyCode.S))
-        {
-            entity.translateY(moveDistance);
+            dashing = true;
+            Point2D move = getMoveDirection().multiply(DASH_DISTANCE);
+            double dashX = entity.getPosition().getX() + move.getX();
+            double dashY = entity.getPosition().getY() + move.getY();
+            dashTarget = new Point2D(dashX, dashY);
         }
 
-        if (input.isHeld(KeyCode.A))
+        if (dashing)
         {
-            entity.translateX(-moveDistance);
+            if (entity.getPosition().distance(dashTarget) <= DASH_SPEED * tpf)
+            {
+                dashing = false;
+                dashCooldown = DASH_COOLDOWN;
+            }
+            else
+            {
+                entity.translateTowards(dashTarget, DASH_SPEED * tpf);
+            }
         }
-        else if (input.isHeld(KeyCode.D))
+        else
         {
-            entity.translateX(moveDistance);
+            double moveDistance = speed * tpf;
+            Point2D move = getMoveDirection().multiply(moveDistance);
+            entity.translate(move);
+            if (dashCooldown > 0)
+            {
+                dashCooldown -= tpf;
+            }
         }
 
         // projectile firing
