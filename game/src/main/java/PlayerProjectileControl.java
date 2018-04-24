d67db18e95101c0613bfbf291c4b0b678c1f45c1
Carson McManus <carson.mcmanus1@gmail.com>
2018-04-24 09:11:48 -0400

    add/fix some javadocs, optimize some imports

--- game/src/main/java/PlayerProjectileControl.java
+++ game/src/main/java/PlayerProjectileControl.java
@@ -3,16 +3,22 @@ package main.java;
 import com.almasb.fxgl.entity.Entity;
 import javafx.geometry.Point2D;
 
+/** A projectile control that homes in on it's target. **/
 public class PlayerProjectileControl extends BaseProjectileControl
 {
     private Entity target;
 
+    /** A projectile control that homes in on it's target.
+     * @param target The entity to target. **/
     public PlayerProjectileControl(Entity target)
     {
         super(new Point2D(0, 0), 1000);
         this.target = target;
     }
 
+    /** A projectile control that homes in on it's target.
+     * @param target The entity to target.
+     * @param critChance Probability that this projectile does critical damage. **/
     public PlayerProjectileControl(Entity target, double critChance)
     {
         super(new Point2D(0, 0), 1000, critChance);
