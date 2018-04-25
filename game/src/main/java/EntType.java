e5058b0da50315a4352ddf889d28403ba96e5e8a
Carson McManus <carson.mcmanus1@gmail.com>
2018-04-25 11:25:27 -0400

    add javadoc to EntType

--- game/src/main/java/EntType.java
+++ game/src/main/java/EntType.java
@@ -2,8 +2,12 @@ package main.java;
 
 public enum EntType
 {
+    /** Indicates the entity is the player. **/
     PLAYER,
+	/** Indicates the entity is a projectile from the player. **/
     PROJECTILE,
+	/** Indicates the entity is the boss. **/
     BOSS,
+	/** Indicates the entity is a projectile from the boss. **/
     BOSS_PROJECTILE
 }
