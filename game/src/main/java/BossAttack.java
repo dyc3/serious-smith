43fa9360696cb3d68e8e53665a2f7494ec8cc2ac
Carson McManus <carson.mcmanus1@gmail.com>
2018-05-11 09:11:43 -0400

    refactor boss attacks to make animated attacks and adding new attacks easier

---
+++ game/src/main/java/BossAttack.java
@@ -0,0 +1,8 @@
+package main.java;
+
+/** Used to specify boss attacks. **/
+public enum BossAttack
+{
+	STAR,
+	BURST
+}
--- game/src/main/java/BossComponent.java
+++ game/src/main/java/BossComponent.java
@@ -5,6 +5,10 @@ import com.almasb.fxgl.entity.SpawnData;
 import com.almasb.fxgl.entity.component.Component;
 import javafx.geometry.Point2D;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 /** Controls the boss. **/
@@ -32,7 +36,14 @@ public class BossComponent extends Component
     /** The minimum amount of time between attacks in seconds. Actual attack intervals may vary
      * depending on previous attack performed. **/
     private double baseAttackInterval;
+    /** Time (in seconds) until the next attack. **/
     private double timeUntilAttack;
+
+    /** Indicates the currently executing attack. Equals `null` when not attacking. **/
+    private BossAttack currentAttack = null;
+    /** Time (in seconds) since the start of the current attack.
+	 * Each attack must determine when it ends on it's own. **/
+    private double attackTime = 0;
 
     private ProjectileFactory projFactory;
 
@@ -55,16 +66,39 @@ public class BossComponent extends Component
     @Override
     public void onUpdate(double tpf)
     {
-        if (timeUntilAttack <= 0)
+        if (currentAttack != null)
         {
-            doAttack();
-            timeUntilAttack = baseAttackInterval;
-        }
+        	doAttack(tpf);
+			attackTime += tpf;
+		}
         else
         {
             timeUntilAttack -= tpf;
+            attackTime = 0;
+
+            if (timeUntilAttack <= 0)
+			{
+				timeUntilAttack = 0;
+				currentAttack = nextAttack();
+
+				System.out.println("Start attack: " + currentAttack);
+			}
         }
     }
+
+    /** Determine the next attack.
+	 * @return The next attack to perform. **/
+    private BossAttack nextAttack()
+	{
+		if (Math.random() < BIG_ATTACK_CHANCE)
+		{
+			return BossAttack.BURST;
+		}
+		else
+		{
+			return BossAttack.STAR;
+		}
+	}
 
     /** Gets the closest player.
 	 * @return Entity of closest player. **/
@@ -95,22 +129,55 @@ public class BossComponent extends Component
 		return closest;
 	}
 
-    /** Determines which attack to perform, then executes it. **/
-    public void doAttack()
+    /** Finds the method that the current attack uses, and execute it with parameters.
+	 * @param tpf Time per frame. **/
+    public void doAttack(double tpf)
 	{
-		double pDoBigAttack = Math.random(); // probability
-		if (pDoBigAttack < BIG_ATTACK_CHANCE)
+		final List<Method> methods = new ArrayList<Method>();
+		Class<?> klass = this.getClass();
+		while (klass != Object.class) // need to iterated thought hierarchy in order to retrieve methods from above the current instance
 		{
-			attackBurst();
+			// iterate though the list of methods declared in the class represented by klass variable, and add those annotated with the specified annotation
+			final List<Method> allMethods = new ArrayList<Method>(Arrays.asList(klass.getMethods()));
+			for (final Method method : allMethods)
+			{
+				HandlesAttack handles = method.getAnnotation(HandlesAttack.class);
+				if (handles != null && handles.attack() == currentAttack)
+				{
+					methods.add(method);
+				}
+			}
+			// move to the upper class in the hierarchy in search for more methods
+			klass = klass.getSuperclass();
 		}
-		else
+
+		System.out.println("found " + methods.size() + " methods");
+
+		// execute all handler methods
+		for (Method m : methods)
 		{
-			attackStar();
+			try
+			{
+				m.invoke(this, tpf);
+			}
+			catch (IllegalAccessException | InvocationTargetException e)
+			{
+				e.printStackTrace();
+			}
 		}
 	}
 
-    /** Fires 8 dumb projectiles around the boss. **/
-    public void attackStar()
+	/** Ends the current attack. Each attack calls this method when they are done. **/
+	private void endAttack()
+	{
+		timeUntilAttack = baseAttackInterval;
+		currentAttack = null;
+	}
+
+    /** Fires 8 dumb projectiles around the boss.
+	 * @param tpf Time per frame. **/
+    @HandlesAttack(attack = BossAttack.STAR)
+    public void attackStar(double tpf)
 	{
 		for (int y = -1; y <= 1; y++)
 		{
@@ -128,10 +195,14 @@ public class BossComponent extends Component
 				getEntity().getWorld().addEntity(projFactory.spawnDumbProjectile(data));
 			}
 		}
+
+		endAttack();
 	}
 
-	/** Fires a shotgun blast of dumb projectiles in the direction of the player. **/
-	public void attackBurst()
+	/** Fires a shotgun blast of dumb projectiles in the direction of the player.
+	 * @param tpf Time per frame. **/
+	@HandlesAttack(attack = BossAttack.BURST)
+	public void attackBurst(double tpf)
 	{
 		Entity target = getPlayer();
 		for (int i = 0; i < BURST_ATTACK_PROJECTILE_COUNT; i++)
@@ -146,5 +217,7 @@ public class BossComponent extends Component
 			data.put("speed", BURST_ATTACK_PROJECTILE_SPEED);
 			getEntity().getWorld().addEntity(projFactory.spawnDumbProjectile(data));
 		}
+
+		endAttack();
 	}
 }
