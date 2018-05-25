04a4836f2e3fad39089d2f544238bafa8a4ccdc2
Carson McManus <dyc3@users.noreply.github.com>
2018-05-25 08:53:03 -0400

    Win lose events (#23)
    * add win/lose events
    
    * add game over menu, add game reset on win/loss

--- game/src/main/java/GameRunner.java
+++ game/src/main/java/GameRunner.java
@@ -9,10 +9,12 @@ import com.almasb.fxgl.entity.Entity;
 import com.almasb.fxgl.entity.RenderLayer;
 import com.almasb.fxgl.entity.SpawnData;
 import com.almasb.fxgl.entity.components.CollidableComponent;
+import com.almasb.fxgl.event.EventTrigger;
 import com.almasb.fxgl.extra.entity.components.HealthComponent;
 import com.almasb.fxgl.entity.components.IDComponent;
 import com.almasb.fxgl.entity.components.IrremovableComponent;
 import com.almasb.fxgl.entity.view.EntityView;
+import com.almasb.fxgl.input.UserAction;
 import com.almasb.fxgl.physics.CollisionHandler;
 import com.almasb.fxgl.scene.Viewport;
 import com.almasb.fxgl.settings.GameSettings;
@@ -20,9 +22,12 @@ import com.almasb.fxgl.ui.ProgressBar;
 import com.gluonhq.charm.down.plugins.audio.Audio;
 import javafx.geometry.Point2D;
 import javafx.scene.effect.BlendMode;
+import javafx.scene.input.MouseButton;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Line;
 import javafx.scene.shape.Rectangle;
+import javafx.scene.text.Text;
+
 import java.util.Map;
 
 /** Main entry class for the application. **/
@@ -54,6 +59,7 @@ public class GameRunner extends GameApplication
 	private static final Color COLOR_BG_GRID = Color.color(0.3, 0.3, 0.3);
 	/** Color of the experience bar. **/
 	private static final Color COLOR_XP_BAR = Color.color(0.2, 0.7, 1);
+	private static final Color COLOR_UI_TEXT = Color.WHITE;
 
 	/** Boss starting health. **/
 	private static final int BOSS_HEALTH = 1000000;
@@ -73,6 +79,27 @@ public class GameRunner extends GameApplication
 	/** Quick reference to the camera holder. **/
 	private Entity camHolder;
 
+	private ProgressBar pbarPlayerHealth;
+	private ProgressBar pbarPlayerXP;
+	private ProgressBar pbarBossHealth;
+	/** The entity that holds pbarBossHealth over the boss. **/
+	private Entity entpbarBossHealth;
+	private Text textGameOver;
+	private Text textStartOver;
+
+	private UserAction actionRestart = new UserAction("restart game") {
+		@Override
+		protected void onActionBegin()
+		{
+			System.out.println("RESTART");
+			textGameOver.setVisible(false);
+			textStartOver.setVisible(false);
+
+			setupGame(true);
+			getInput().rebind(getInput().getActionByName("restart game"), MouseButton.NONE);
+		}
+	};
+
     /** Program entry.
 	 * @param args command line arguments. **/
     public static void main(String[] args)
@@ -89,6 +116,104 @@ public class GameRunner extends GameApplication
         settings.setTitle(WINDOW_TITLE);
         settings.setVersion(VERSION);
     }
+
+    /** Set up the game objects for a new game.
+	 * @param restart Set to false if first time set up. Set to true to reset the game (after game end). **/
+    protected void setupGame(boolean restart)
+	{
+		if (restart)
+		{
+			if (player != null)
+			{
+				player.removeFromWorld();
+			}
+			if (boss != null)
+			{
+				boss.removeFromWorld();
+			}
+			camHolder.removeFromWorld();
+
+			for (Entity entity : getGameWorld().getEntitiesByType(EntType.PROJECTILE))
+			{
+				entity.removeFromWorld();
+			}
+			for (Entity entity : getGameWorld().getEntitiesByType(EntType.BOSS_PROJECTILE))
+			{
+				entity.removeFromWorld();
+			}
+			for (Entity entity : getGameWorld().getEntitiesByType(EntType.XP_ORB))
+			{
+				entity.removeFromWorld();
+			}
+			for (Entity entity : getGameWorld().getEntitiesByType(EntType.BOSS_LASER))
+			{
+				entity.removeFromWorld();
+			}
+		}
+
+		ProjectileFactory projectileFactory = new ProjectileFactory();
+
+		Rectangle rectPlayer = new Rectangle(0, 0, PLAYER_SIZE, PLAYER_SIZE);
+		rectPlayer.setFill(Color.BLUE);
+		player = Entities.builder()
+				.type(EntType.PLAYER)
+				.at(0, 300)
+				.viewFromNodeWithBBox(rectPlayer)
+				.with(new HealthComponent(100))
+				.with(new IDComponent("player", 0))
+				.with(new PlayerComponent(getInput(), projectileFactory))
+				.with(new CollidableComponent(true))
+				.buildAndAttach(getGameWorld());
+
+		Rectangle rectBoss = new Rectangle(0, 0, BOSS_SIZE, BOSS_SIZE);
+		rectBoss.setFill(Color.RED);
+		boss = Entities.builder()
+				.type(EntType.BOSS)
+				.at(0, 0)
+				.viewFromNodeWithBBox(rectBoss)
+				.with(new HealthComponent(BOSS_HEALTH))
+				.with(new BossComponent(projectileFactory))
+				.with(new IDComponent("boss", 0))
+				.with(new CollidableComponent(true))
+				.buildAndAttach(getGameWorld());
+
+		camHolder = Entities.builder()
+				.with(new IDComponent("camera holder", 0))
+				.with(new ParentFollowerComponent(player, 0, 0))
+				.with(new CameraShakerComponent())
+				.buildAndAttach(getGameWorld());
+
+		Viewport viewport = getGameScene().getViewport();
+		viewport.bindToEntity(camHolder,
+				(getWidth() / 2) - (player.getWidth() / 2),
+				(getHeight() / 2) - (player.getHeight() / 2));
+
+		if (restart)
+		{
+			PlayerComponent player = this.player.getComponent(PlayerComponent.class);
+			pbarPlayerHealth.currentValueProperty()
+					.bind(this.player.getComponent(HealthComponent.class).valueProperty());
+			pbarPlayerHealth.maxValueProperty().bind(player.getMaxHealthProperty());
+			pbarPlayerXP.currentValueProperty().bind(player.getXpProperty());
+			pbarPlayerXP.maxValueProperty().bind(player.getXpToNextLevelBinding());
+			pbarBossHealth.currentValueProperty().bind(boss.getComponent(HealthComponent.class).valueProperty());
+			entpbarBossHealth.getComponent(ParentFollowerComponent.class).setTarget(boss);
+		}
+
+		// set up win/lose events
+		getEventBus().addEventTrigger(new EventTrigger<>(
+				() -> boss.getComponent(HealthComponent.class).getValue() <= 0,
+				() -> new GameEndEvent(GameEndEvent.WIN, true)
+		));
+
+		getEventBus().addEventTrigger(new EventTrigger<>(
+				() -> player.getComponent(HealthComponent.class).getValue() <= 0,
+				() -> new GameEndEvent(GameEndEvent.LOSE, false)
+		));
+
+		getEventBus().addEventHandler(GameEndEvent.WIN, event -> onWin());
+		getEventBus().addEventHandler(GameEndEvent.LOSE, event -> onLose());
+	}
 
     /** Initialize the game. Sets up background and builds player and boss entities. Binds camera to player. **/
     @Override
@@ -118,46 +243,14 @@ public class GameRunner extends GameApplication
                 .with(new IrremovableComponent())
                 .buildAndAttach(getGameWorld());
 
-        Rectangle rectPlayer = new Rectangle(0, 0, PLAYER_SIZE, PLAYER_SIZE);
-        rectPlayer.setFill(Color.BLUE);
-        player = Entities.builder()
-                .type(EntType.PLAYER)
-                .at(0, 300)
-                .viewFromNodeWithBBox(rectPlayer)
-                .with(new HealthComponent(100))
-				.with(new IDComponent("player", 0))
-				.with(new PlayerComponent(getInput(), projectileFactory))
-                .with(new CollidableComponent(true))
-                .buildAndAttach(getGameWorld());
-
-        Rectangle rectBoss = new Rectangle(0, 0, BOSS_SIZE, BOSS_SIZE);
-        rectBoss.setFill(Color.RED);
-        boss = Entities.builder()
-                .type(EntType.BOSS)
-                .at(0, 0)
-                .viewFromNodeWithBBox(rectBoss)
-                .with(new HealthComponent(BOSS_HEALTH))
-                .with(new BossComponent(projectileFactory))
-                .with(new IDComponent("boss", 0))
-                .with(new CollidableComponent(true))
-                .buildAndAttach(getGameWorld());
-
-		camHolder = Entities.builder()
-				.with(new IDComponent("camera holder", 0))
-				.with(new ParentFollowerComponent(player, 0, 0))
-				.with(new CameraShakerComponent())
-				.buildAndAttach(getGameWorld());
-
-        Viewport viewport = getGameScene().getViewport();
-        viewport.bindToEntity(camHolder,
-                       (getWidth() / 2) - (player.getWidth() / 2),
-                       (getHeight() / 2) - (player.getHeight() / 2));
+		setupGame(false);
     }
 
     @Override
     protected void initInput()
     {
 		// NOTE: We handle all player controls in PlayerComponent.
+		getInput().addAction(actionRestart, MouseButton.NONE);
     }
 
     /** Initializes UI elements, including health bars hovering over entities in the world. **/
@@ -165,40 +258,52 @@ public class GameRunner extends GameApplication
     protected void initUI()
     {
     	int hudElementCount = 0;
-    	PlayerComponent p = player.getComponent(PlayerComponent.class);
+    	PlayerComponent player = this.player.getComponent(PlayerComponent.class);
 
-        ProgressBar pbarPlayerHealth = new ProgressBar();
+        pbarPlayerHealth = new ProgressBar();
         pbarPlayerHealth.setTranslateX(UI_HUD_OFFSET_X);
         pbarPlayerHealth.setTranslateY(UI_HUD_OFFSET_Y + (UI_HUD_SPACING_Y * hudElementCount++));
         pbarPlayerHealth.makeHPBar();
-        pbarPlayerHealth.currentValueProperty().bind(player.getComponent(HealthComponent.class).valueProperty());
-		pbarPlayerHealth.maxValueProperty().bind(p.getMaxHealthProperty());
+        pbarPlayerHealth.currentValueProperty().bind(this.player.getComponent(HealthComponent.class).valueProperty());
+		pbarPlayerHealth.maxValueProperty().bind(player.getMaxHealthProperty());
 
-		ProgressBar pbarPlayerXP = new ProgressBar();
+		pbarPlayerXP = new ProgressBar();
 		pbarPlayerXP.setTranslateX(UI_HUD_OFFSET_X);
 		pbarPlayerXP.setTranslateY(UI_HUD_OFFSET_Y + (UI_HUD_SPACING_Y * hudElementCount++));
 		pbarPlayerXP.setFill(COLOR_XP_BAR);
 		pbarPlayerXP.setMinValue(0);
-		pbarPlayerXP.currentValueProperty().bind(p.getXpProperty());
-		pbarPlayerXP.maxValueProperty().bind(p.getXpToNextLevelBinding());
+		pbarPlayerXP.currentValueProperty().bind(player.getXpProperty());
+		pbarPlayerXP.maxValueProperty().bind(player.getXpToNextLevelBinding());
 
 		getGameScene().addUINode(pbarPlayerHealth);
 		getGameScene().addUINode(pbarPlayerXP);
 
-        ProgressBar pbarBossHealth = new ProgressBar();
+        pbarBossHealth = new ProgressBar();
         pbarBossHealth.setWidth(UI_HUD_BOSS_HEALTH_BAR_WIDTH);
         pbarBossHealth.makeHPBar();
 		pbarBossHealth.setMaxValue(BOSS_HEALTH);
 		pbarBossHealth.setLabelVisible(true);
         pbarBossHealth.currentValueProperty().bind(boss.getComponent(HealthComponent.class).valueProperty());
 
-        Entities.builder()
+		entpbarBossHealth = Entities.builder()
                 .viewFromNode(pbarBossHealth)
 				.with(new IrremovableComponent())
                 .with(new ParentFollowerComponent(boss,
 						(BOSS_SIZE - UI_HUD_BOSS_HEALTH_BAR_WIDTH) / 2,
 						UI_HUD_BOSS_HEALTH_BAR_OFFSET_Y))
                 .buildAndAttach(getGameWorld());
+
+		textGameOver = getUIFactory().newText("Game Over", COLOR_UI_TEXT, 26);
+		textGameOver.setX(getWidth() / 2 - 100);
+		textGameOver.setY(getHeight() / 2 - 100);
+		textGameOver.setVisible(false);
+		getGameScene().addUINode(textGameOver);
+
+		textStartOver = getUIFactory().newText("Click to try again.", COLOR_UI_TEXT, 18);
+		textStartOver.setX(getWidth() / 2 - 100);
+		textStartOver.setY(getHeight() / 2 + 100);
+		textStartOver.setVisible(false);
+		getGameScene().addUINode(textStartOver);
 
 		// Play the background music.
 		// Normally, we should just run `getAudioPlayer().loopBGM("intense.mp3")`, but
@@ -337,4 +442,30 @@ public class GameRunner extends GameApplication
 			}
 		});
     }
+
+    /** Called when the game is won. **/
+	protected void onWin()
+	{
+		System.out.println("Game won");
+		boss.removeFromWorld();
+
+		textGameOver.setText("You Win!");
+		textGameOver.setVisible(true);
+		textStartOver.setVisible(true);
+
+		getInput().rebind(actionRestart, MouseButton.PRIMARY);
+	}
+
+	/** Called when the game is lost. **/
+	protected void onLose()
+	{
+		System.out.println("Game lost");
+		player.removeFromWorld();
+
+		textGameOver.setText("Game Over");
+		textGameOver.setVisible(true);
+		textStartOver.setVisible(true);
+
+		getInput().rebind(actionRestart, MouseButton.PRIMARY);
+	}
 }
